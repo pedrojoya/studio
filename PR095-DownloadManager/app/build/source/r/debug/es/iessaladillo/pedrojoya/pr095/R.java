6eff8a0eed27ae4cd8f7f7f6de3a332b1819c01a
Pedro Joya <pedrojoya@gmail.com>
2014-04-11 14:16:14 +0200

    PR095_terminado

--- PR095-DownloadManager/app/build/source/r/debug/es/iessaladillo/pedrojoya/pr095/R.java
+++ PR095-DownloadManager/app/build/source/r/debug/es/iessaladillo/pedrojoya/pr095/R.java
@@ -18,20 +18,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int btnDescargar=0x7f080000;
+        public static final int imgAnterior=0x7f080002;
+        public static final int imgPlay=0x7f080003;
+        public static final int imgSiguiente=0x7f080004;
+        public static final int lstCanciones=0x7f080000;
+        public static final int mnuDescargas=0x7f080005;
+        public static final int rlPanel=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int descargar=0x7f050002;
-        public static final int descargar_y_reproducir=0x7f050003;
+        public static final int anterior=0x7f050001;
+        public static final int app_name=0x7f050002;
+        public static final int descargando=0x7f050003;
+        public static final int descargar=0x7f050004;
+        public static final int descargar_y_reproducir=0x7f050005;
+        public static final int descargas=0x7f050006;
+        public static final int reproducir_o_pausar=0x7f050007;
+        public static final int siguiente=0x7f050008;
     }
     public static final class style {
         /**  Customize your theme here. 
--- PR095-DownloadManager/app/src/main/java/es/iessaladillo/pedrojoya/pr095/MainActivity.java
+++ PR095-DownloadManager/app/src/main/java/es/iessaladillo/pedrojoya/pr095/MainActivity.java
@@ -13,22 +13,41 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.util.ArrayList;
 
-public class MainActivity extends Activity implements View.OnClickListener, MediaPlayer.OnPreparedListener {
 
+public class MainActivity extends Activity implements View.OnClickListener,
+        MediaPlayer.OnPreparedListener,
+        AdapterView.OnItemClickListener {
+
+    // Variables.
     private DownloadManager mGestor;
     private BroadcastReceiver mReceptor;
     private MediaPlayer mReproductor;
+    private int mPosCancionActual = -1;
+
+    // Vistas.
+    private ImageView imgPlay;
+    private ListView lstCanciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btnDescargar).setOnClickListener(this);
-        // Se obtiene el mGestor de descargas.
+        getVistas();
+        // Se crea el reproductor.
+        mReproductor = new MediaPlayer();
+        // Se obtiene el gestor de descargas.
         mGestor = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
         // Se crea el receptor de eventos relacionados con la descarga.
         mReceptor = new BroadcastReceiver() {
@@ -37,43 +56,67 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
             public void onReceive(Context context, Intent intent) {
                 // Se obtiene la acción del intent.
                 String accion = intent.getAction();
-                // Si se ha completado la descarga.
-                if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(accion)) {
-                    // Se obtiene el id de la descarga.
-                    long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
-                    // Se realiza la consulta para obtener los datos de la descarga.
-                    DownloadManager.Query consulta = new DownloadManager.Query();
-                    consulta.setFilterById(downloadId);
-                    Cursor c = mGestor.query(consulta);
-                    if (c != null) {
-                        if (c.moveToFirst()) {
-                            // Dependiendo del estado de la descarga.
-                            int estado = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
-                            switch (estado) {
-                                case DownloadManager.STATUS_SUCCESSFUL:
-                                    // Se reproduce la canción a partir de su Uri local.
-                                    String sUri = c.getString(
-                                            c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
-                                    reproducir(sUri);
-                                    break;
-                                case DownloadManager.STATUS_FAILED:
-                                    // Se informe del error.
-                                    String motivo = c.getString(
-                                            c.getColumnIndex(DownloadManager.COLUMN_REASON));
-                                    informarError(motivo);
-                                    break;
-                            }
+                // Se obtiene el id de la descarga.
+                long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+                // Se realiza la consulta para obtener los datos de la descarga.
+                DownloadManager.Query consulta = new DownloadManager.Query();
+                consulta.setFilterById(downloadId);
+                Cursor c = mGestor.query(consulta);
+                if (c != null) {
+                    if (c.moveToFirst()) {
+                        // Dependiendo del estado de la descarga.
+                        int estado = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                        switch (estado) {
+                            case DownloadManager.STATUS_SUCCESSFUL:
+                                // Se reproduce la canción a partir de su Uri local.
+                                String sUri = c.getString(
+                                        c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                                reproducir(sUri);
+                                break;
+                            case DownloadManager.STATUS_FAILED:
+                                // Se informe del error.
+                                String motivo = c.getString(
+                                        c.getColumnIndex(DownloadManager.COLUMN_REASON));
+                                informarError(motivo);
+                                break;
                         }
                     }
-                }
-                // Si se ha pulsado sobre una notificación.
-                else {
-                    // Se muestra la actividad estándar de descargas.
-                    Intent i = new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS);
-                    startActivity(i);
+                    // Se cierra el cursor.
+                    c.close();
                 }
             }
         };
+    }
+
+    // Muestra la actividad estándar de descargas.
+    private void mostrarDescargas() {
+        // Se muestra la actividad estándar de descargas.
+        Intent i = new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mReproductor.release();
+        mReproductor = null;
+        super.onDestroy();
+    }
+
+    // Obtiene e inicializa las vistas.
+    private void getVistas() {
+        ImageView imgAnterior = (ImageView) findViewById(R.id.imgAnterior);
+        imgAnterior.setOnClickListener(this);
+        ImageView imgSiguiente = (ImageView) findViewById(R.id.imgSiguiente);
+        imgSiguiente.setOnClickListener(this);
+        imgPlay = (ImageView) findViewById(R.id.imgPlay);
+        imgPlay.setOnClickListener(this);
+        lstCanciones = (ListView) findViewById(R.id.lstCanciones);
+        ArrayList<Cancion> canciones = getListaCanciones();
+        ArrayAdapter<Cancion> adaptador = new ArrayAdapter<Cancion>(this,
+                android.R.layout.simple_list_item_activated_1, canciones);
+        lstCanciones.setAdapter(adaptador);
+        lstCanciones.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        lstCanciones.setOnItemClickListener(this);
     }
 
     // Informa al usuario del motivo del error en la descarga.
@@ -84,9 +127,9 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
     // Reproduce el audio en base a la uri recibida.
     private void reproducir(String sUri) {
 // Se crea el mReproductor.
-        mReproductor = new MediaPlayer();
         try {
             // Se establecen sus propiedades.
+            mReproductor.reset();
             mReproductor.setDataSource(this, Uri.parse(sUri)); // Fuente.
             mReproductor.setAudioStreamType(AudioManager.STREAM_MUSIC); // Música.
             // La actividad actuará como listener cuando el mReproductor esté preparado.
@@ -114,28 +157,150 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
         unregisterReceiver(mReceptor);
     }
 
-    @Override
-    public void onClick(View view) {
+    // Descarga una canción.
+    private void descargar(Cancion cancion) {
         // Se crea la solicitud de descarga.
         DownloadManager.Request solicitud = new DownloadManager.Request(
-                Uri.parse("https://www.youtube.com/audiolibrary_download?vid=fafb35a907cd6e730"));
+                Uri.parse(cancion.getUrl()));
         solicitud.setAllowedNetworkTypes(
                 DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
         solicitud.setAllowedOverRoaming(false);
-        solicitud.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "carmen.mp4");
+        solicitud.setDestinationInExternalPublicDir(Environment.DIRECTORY_MUSIC, cancion.getNombre() + ".mp4");
         // solicitud.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, "carmen.mp4");
-        solicitud.setTitle("Les Toreadors");
-        solicitud.setDescription("Les Toreadors from Carmen (by Bizet)");
+        solicitud.setTitle(cancion.getNombre());
+        solicitud.setDescription(cancion.getNombre() + "(" + cancion.getDuracion() + ")");
         solicitud.allowScanningByMediaScanner();
         solicitud.setNotificationVisibility(
-                DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                DownloadManager.Request.VISIBILITY_VISIBLE);
         // Se encola la solicitud.
         mGestor.enqueue(solicitud);
+        Toast.makeText(this, getString(R.string.descargando), Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onPrepared(MediaPlayer mediaPlayer) {
         // Se inicia la reproducción.
         mReproductor.start();
+        // Se actualiza el icono al de pausar.
+        imgPlay.setImageResource(android.R.drawable.ic_media_pause);
     }
+
+    // Cuando se pulsa en una canción de la lista.
+    @Override
+    public void onItemClick(AdapterView<?> lst, View v, int position, long id) {
+        // Se descarga la canción.
+        reproducirCancion(position);
+    }
+
+    // Descarga la canción correspondiente a una posición.
+    private void reproducirCancion(int position) {
+        mPosCancionActual = position;
+        lstCanciones.setItemChecked(position, true);
+        Cancion cancion = (Cancion)lstCanciones.getItemAtPosition(position);
+        File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+        File fichero = new File(directory, cancion.getNombre() + ".mp4");
+        if (fichero.exists()) {
+            reproducir(Uri.fromFile(fichero).toString());
+        }
+        else {
+            descargar(cancion);
+        }
+    }
+
+    // Cuando se pulsa en algún botón del panel.
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imgAnterior:
+                imgAnteriorOnClick();
+                break;
+            case R.id.imgSiguiente:
+                imgSiguienteOnClick();
+                break;
+            case R.id.imgPlay:
+                imgPlayOnClick();
+                break;
+        }
+    }
+
+    // Al pulsar sobre reproducir / pausar.
+    private void imgPlayOnClick() {
+            // Si ya se está reproduciendo.
+            if (mPosCancionActual >= 0 && mReproductor.isPlaying()) {
+                // Se pausa la reproducción.
+                mReproductor.pause();
+                // Se actualiza el icono al de reproducir.
+                imgPlay.setImageResource(android.R.drawable.ic_media_play);
+            } else {
+                // Si estábamos en pausa.
+                if (mPosCancionActual >= 0) {
+                    // Se continua la reproducción.
+                    mReproductor.start();
+                } else {
+                    // Se comienza la reproducción de la primera canción de la
+                    // lista.
+                    reproducirCancion(0);
+                }
+                // Se actualiza el icono al de pausar.
+                imgPlay.setImageResource(android.R.drawable.ic_media_pause);
+            }
+    }
+
+    // Al pulsar sobre Siguiente.
+    private void imgSiguienteOnClick() {
+        reproducirCancion((mPosCancionActual + 1) % lstCanciones.getCount());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mnuDescargas:
+                mostrarDescargas();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Al pulsar sobre Anterior.
+    private void imgAnteriorOnClick() {
+        int anterior;
+        if (mPosCancionActual >= 0) {
+            anterior = mPosCancionActual - 1;
+            if (anterior < 0) {
+                anterior = lstCanciones.getCount() - 1;
+            }
+        } else {
+            anterior = 0;
+        }
+        reproducirCancion(anterior);
+    }
+
+    // Crea y retorna el ArrayList de canciones.
+    private ArrayList<Cancion> getListaCanciones() {
+        ArrayList<Cancion> canciones = new ArrayList<Cancion>();
+        canciones
+                .add(new Cancion("Morning Mood (by Grieg)", "3:43",
+                        "https://www.youtube.com/audiolibrary_download?vid=036500ffbf472dcc"));
+        canciones
+                .add(new Cancion("Brahms Lullaby", "1:46",
+                        "https://www.youtube.com/audiolibrary_download?vid=9894a50b486c6136"));
+        canciones
+                .add(new Cancion("From Russia With Love", "2:26",
+                        "https://www.youtube.com/audiolibrary_download?vid=4e8d1a0fdb3bbe12"));
+        canciones
+                .add(new Cancion("Les Toreadors from Carmen (by Bizet)",
+                        "2:21",
+                        "https://www.youtube.com/audiolibrary_download?vid=fafb35a907cd6e73"));
+        canciones
+                .add(new Cancion("Funeral March (by Chopin)", "9:25",
+                        "https://www.youtube.com/audiolibrary_download?vid=4a7d058f20d31cc4"));
+        return canciones;
+    }
+
 }
