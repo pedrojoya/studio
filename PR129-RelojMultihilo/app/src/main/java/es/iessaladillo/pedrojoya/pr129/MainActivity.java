6bd6227d97cc699ddba0feb55ac6ce9a4a7f86d7
Pedro Joya <pedrojoya@gmail.com>
2015-04-06 14:40:30 +0200

    pr129

---
+++ PR129-RelojMultihilo/app/src/main/java/es/iessaladillo/pedrojoya/pr129/MainActivity.java
@@ -0,0 +1,102 @@
+package es.iessaladillo.pedrojoya.pr129;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+@SuppressLint("SimpleDateFormat")
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+    private Button btnIniciar;
+    private Thread hiloSecundario;
+    private TextView lblTiempo;
+    private final SimpleDateFormat formateador = new SimpleDateFormat("HH:mm:ss");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getVistas();
+    }
+
+    private void getVistas() {
+        lblTiempo = (TextView) this.findViewById(R.id.lblTiempo);
+        lblTiempo.setText(formateador.format(new Date()));
+        btnIniciar = (Button) this.findViewById(R.id.btnIniciar);
+        btnIniciar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Dependiendo del botón pulsado.
+        switch (v.getId()) {
+        case R.id.btnIniciar:
+            if (btnIniciar.getText().toString()
+                    .equals(getString(R.string.iniciar))) {
+                iniciar();
+            } else {
+                parar();
+            }
+            break;
+        }
+    }
+
+    private void iniciar() {
+        // Se crea el hilo pasándole el tiempo guardado al objeto Crono.
+        hiloSecundario = new Thread(new Reloj(), "Secundario");
+        // Se inicia el hilo.
+        hiloSecundario.start();
+        // Se cambia el texto del botón.
+        btnIniciar.setText(R.string.parar);
+    }
+
+    private void parar() {
+        // Se interrumple el hilo.
+        hiloSecundario.interrupt();
+        // Se cambia el texto del botón.
+        btnIniciar.setText(R.string.iniciar);
+    }
+
+    private class Reloj implements Runnable {
+
+        // Variables a nivel de clase.
+        final SimpleDateFormat formateador = new SimpleDateFormat("HH:mm:ss");
+
+        @Override
+        public void run() {
+            while (true) {
+                // Se obtiene la representación en cadena de la hora actual.
+                // La variable debe ser final para que se pueda enviar en el
+                // Runnable que se envía al hilo principal.
+                final String cadena = formateador.format(new Date());
+                // Se envía la actualización al hilo principal.
+                lblTiempo.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        lblTiempo.setText(cadena);
+                    }
+
+                });
+                // Espera un segundo.
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // Se finaliza el hilo si se produce la interrupción
+                    // mientras se duerme.
+                    return;
+                }
+            }
+        }
+
+    }
+
+}
