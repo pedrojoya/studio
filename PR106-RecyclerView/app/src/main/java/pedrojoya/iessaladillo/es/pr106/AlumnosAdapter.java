e0fc87b4a3625a79d42ff3adb1a859974d91f302
pedrojoya <pedrojoya@gmail.com>
2015-04-28 11:22:59 +0200

    pr106 context onItemClick

--- PR106-RecyclerView/app/src/main/java/pedrojoya/iessaladillo/es/pr106/AlumnosAdapter.java
+++ PR106-RecyclerView/app/src/main/java/pedrojoya/iessaladillo/es/pr106/AlumnosAdapter.java
@@ -17,65 +17,108 @@ import de.hdodenhof.circleimageview.CircleImageView;
 // Adaptador para la lista de alumnos.
 public class AlumnosAdapter extends RecyclerView.Adapter<AlumnosAdapter.ViewHolder> {
 
-    private final Context context;
-    private final ArrayList<Alumno> datos;
-    private final LayoutInflater inflater;
+    private final ArrayList<Alumno> mDatos;
     private View emptyView;
     private OnItemLongClickListener onItemLongClickListener;
     private OnItemClickListener onItemClickListener;
 
+    // Constructor.
     public AlumnosAdapter(Context context, ArrayList<Alumno> datos) {
-        this.context = context;
-        this.datos = datos;
-        // Se obtiene un inflador de layouts.
-        inflater = LayoutInflater.from(context);
+        mDatos = datos;
     }
 
     // Cuando se debe crear una nueva vista para el elemento.
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // Se infla la especificación XML para obtener la vista-fila.
-        View view = inflater.inflate(R.layout.activity_main_item, parent, false);
-        // Se crea y retorna el contenedor de vistas para la fila.
-        return new ViewHolder(view);
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_main_item, parent, false);
+        // Se crea el contenedor de vistas para la fila.
+        final ViewHolder viewHolder = new ViewHolder(itemView);
+
+        // Se establecen los listener de la vista correspondiente al ítem
+        // y de las subvistas.
+
+        // Cuando se hace click sobre el elemento.
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onItemClickListener != null) {
+                    // Se informa al listener.
+                    onItemClickListener.onItemClick(v,
+                            mDatos.get(viewHolder.getAdapterPosition()),
+                            viewHolder.getAdapterPosition());
+                }
+            }
+        });
+        // Cuando se hace click largo sobre el elemento.
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (onItemLongClickListener != null) {
+                    // Se informa al listener.
+                    onItemLongClickListener.onItemLongClick(v,
+                            mDatos.get(viewHolder.getAdapterPosition()),
+                            viewHolder.getAdapterPosition());
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        // Cuando se pulsa sobre bajar.
+        viewHolder.btnDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Se mueve el alumno una posición hacia abajo.
+                int position = viewHolder.getAdapterPosition();
+                if (position < mDatos.size() - 1) {
+                    swapItems(position, position + 1);
+                }
+            }
+        });
+        // Se retorna el contenedor.
+        return viewHolder;
     }
 
-    // Cuando se deben escribir los datos en la vista del elemento.
+    // Cuando se deben escribir los datos en las subvistas de la
+    // vista correspondiente al ítem.
     @Override
     public void onBindViewHolder(AlumnosAdapter.ViewHolder holder, int position) {
         // Se obtiene el alumno correspondiente.
-        Alumno alumno = datos.get(position);
-        // Se escriben los datos en la vista.
+        Alumno alumno = mDatos.get(position);
+        // Se escriben los mDatos en la vista.
         holder.lblNombre.setText(alumno.getNombre());
         holder.lblDireccion.setText(alumno.getDireccion());
-        Picasso.with(context)
+        Picasso.with(holder.imgAvatar.getContext())
                 .load(alumno.getUrlFoto())
                 .into(holder.imgAvatar);
         // Se muestran u ocultan el botón de movimiento.
-        holder.btnDown.setVisibility(position < datos.size() - 1 ? View.VISIBLE : View.GONE);
+        holder.btnDown.setVisibility(position < mDatos.size() - 1 ?
+                View.VISIBLE : View.GONE);
 
     }
 
-    // Retorna el número de ítems.
+    // Retorna el número de ítems gestionados.
     @Override
     public int getItemCount() {
-        return datos.size();
+        return mDatos.size();
     }
 
     // Elimina un elemento de la lista.
     public void removeItem(int position) {
-        datos.remove(position);
+        mDatos.remove(position);
         notifyItemRemoved(position);
 /*
         if (position > 0) {
             notifyItemChanged(position - 1);
         }
-        if (position == 0 && datos.size() > 0) {
+        if (position == 0 && mDatos.size() > 0) {
             notifyItemChanged(0);
         }
-*/      // Si aún hay datos y se ha eliminado el último elemento, se notifica que ha habido cambios
+*/      // Si aún hay mDatos y se ha eliminado el último elemento, se notifica que ha habido cambios
         // en el elemento anterior para que se oculte la flecha.
-        if (datos.size() > 0 && position == datos.size()) {
+        if (mDatos.size() > 0 && position == mDatos.size()) {
             notifyItemChanged(position - 1);
         }
         // Si la lista ha quedado vacía se muestra la empty view.
@@ -85,12 +128,12 @@ public class AlumnosAdapter extends RecyclerView.Adapter<AlumnosAdapter.ViewHold
     // Añade un elemento a la lista.
     public void addItem(Alumno alumno) {
         // Se añade el elemento.
-        datos.add(alumno);
+        mDatos.add(alumno);
         // Se notifica que se ha insertado un elemento en la última posición.
-        notifyItemInserted(datos.size() - 1);
+        notifyItemInserted(mDatos.size() - 1);
         // Se notifica que ha habido cambios en el elemento anterior, para que se muestre la flecha.
-        if (datos.size() > 1) {
-            notifyItemChanged(datos.size() - 2);
+        if (mDatos.size() > 1) {
+            notifyItemChanged(mDatos.size() - 2);
         }
         // Si la lista estaba vacía se oculta la empty view.
         checkIfEmpty();
@@ -99,14 +142,14 @@ public class AlumnosAdapter extends RecyclerView.Adapter<AlumnosAdapter.ViewHold
     // Intercambia dos elementos de la lista.
     void swapItems(int from, int to) {
         // Se realiza el intercambio.
-        Alumno alumnoTo = datos.get(to);
-        datos.set(to, datos.get(from));
-        datos.set(from, alumnoTo);
+        Alumno alumnoTo = mDatos.get(to);
+        mDatos.set(to, mDatos.get(from));
+        mDatos.set(from, alumnoTo);
         // Se notifica el movimiento.
         notifyItemMoved(from, to);
         // Si en el intercambio ha participado el último elemento se notifica que ha habido
         // cambios en los elementos para que se muestren u oculten adecuadamente las flechas.
-        if (from == datos.size() - 1 || to == datos.size() - 1) {
+        if (from == mDatos.size() - 1 || to == mDatos.size() - 1) {
             notifyItemChanged(from);
             notifyItemChanged(to);
         }
@@ -138,7 +181,7 @@ public class AlumnosAdapter extends RecyclerView.Adapter<AlumnosAdapter.ViewHold
     }
 
     // Contenedor de vistas para la vista-fila.
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    static class ViewHolder extends RecyclerView.ViewHolder {
 
         // El contenedor de vistas para un elemento de la lista debe contener...
         private final TextView lblNombre;
@@ -155,44 +198,6 @@ public class AlumnosAdapter extends RecyclerView.Adapter<AlumnosAdapter.ViewHold
             lblDireccion = (TextView) itemView.findViewById(R.id.lblDireccion);
             imgAvatar = (CircleImageView) itemView.findViewById(R.id.imgAvatar);
             btnDown = (ImageButton) itemView.findViewById(R.id.btnDown);
-            // Cuando se hace click sobre el elemento.
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (onItemClickListener != null) {
-                        // Se informa al listener.
-                        onItemClickListener.onItemClick(v, datos.get(getAdapterPosition()),
-                                getAdapterPosition());
-                    }
-                }
-            });
-            // Cuando se hace click largo sobre el elemento.
-            itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    if (onItemLongClickListener != null) {
-                        // Se informa al listener.
-                        onItemLongClickListener.onItemLongClick(v, datos.get(getAdapterPosition()),
-                                getAdapterPosition());
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            });
-
-            // Cuando se pulsa sobre bajar.
-            btnDown.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Se mueve el alumno una posición hacia abajo.
-                    int position = getAdapterPosition();
-                    if (position < datos.size() - 1) {
-                        swapItems(position, position + 1);
-                    }
-                }
-            });
-
         }
 
     }
