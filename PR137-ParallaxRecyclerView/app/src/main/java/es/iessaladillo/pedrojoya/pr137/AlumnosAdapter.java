4338f845f86dbdb5665a651de7243b10fc09c272
pedrojoya <pedrojoya@gmail.com>
2015-05-11 07:56:49 +0200

    pr137

---
+++ PR137-ParallaxRecyclerView/app/src/main/java/es/iessaladillo/pedrojoya/pr137/AlumnosAdapter.java
@@ -0,0 +1,281 @@
+package es.iessaladillo.pedrojoya.pr137;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+// Adaptador para la lista de alumnos.
+public class AlumnosAdapter extends RecyclerView.Adapter<AlumnosAdapter.ViewHolder> {
+
+    // Interfaz que debe implementar el listener para cuando se haga click
+    // sobre un elemento.
+    public interface OnItemClickListener {
+        void onItemClick(View view, Alumno alumno, int position);
+    }
+
+    // Interfaz que debe implementar el listener para cuando se haga click
+    // largo sobre un elemento.
+    public interface OnItemLongClickListener {
+        void onItemLongClick(View view, Alumno alumno, int position);
+    }
+
+    // Interfaz que debe implementar el listener para cuando la lista pase a
+    // o deje de estar vacía.
+    public interface OnEmptyStateChangedListener {
+        void onEmptyStateChanged(boolean isEmpty);
+    }
+
+    private final ArrayList<Alumno> mDatos;
+    private OnItemLongClickListener mOnItemLongClickListener;
+    private OnItemClickListener mOnItemClickListener;
+    private OnEmptyStateChangedListener mOnEmptyStateChangedListener;
+    private SparseBooleanArray mSelectedItems = new SparseBooleanArray();
+    private boolean mIsEmpty = true;
+
+
+    // Constructor.
+    public AlumnosAdapter(ArrayList<Alumno> datos) {
+        mDatos = datos;
+    }
+
+    // Cuando se debe crear una nueva vista para el elemento.
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Se infla la especificación XML para obtener la vista-fila.
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_main_item, parent, false);
+        // Se crea el contenedor de vistas para la fila.
+        final ViewHolder viewHolder = new ViewHolder(itemView);
+
+        // Se establecen los listener de la vista correspondiente al ítem
+        // y de las subvistas.
+
+        // Cuando se hace click sobre el elemento.
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnItemClickListener != null) {
+                    // Se informa al listener.
+                    mOnItemClickListener.onItemClick(v,
+                            mDatos.get(viewHolder.getAdapterPosition()),
+                            viewHolder.getAdapterPosition());
+                }
+            }
+        });
+        // Cuando se hace click largo sobre el elemento.
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (mOnItemLongClickListener != null) {
+                    // Se informa al listener.
+                    mOnItemLongClickListener.onItemLongClick(v,
+                            mDatos.get(viewHolder.getAdapterPosition()),
+                            viewHolder.getAdapterPosition());
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        // Se retorna el contenedor.
+        return viewHolder;
+    }
+
+    // Cuando se deben escribir los datos en las subvistas de la
+    // vista correspondiente al ítem.
+    @Override
+    public void onBindViewHolder(AlumnosAdapter.ViewHolder holder, int position) {
+        // Se obtiene el alumno correspondiente.
+        Alumno alumno = mDatos.get(position);
+        // Se escriben los mDatos en la vista.
+        holder.lblNombre.setText(alumno.getNombre());
+        holder.lblDireccion.setText(alumno.getDireccion());
+        Picasso.with(holder.imgAvatar.getContext())
+                .load(alumno.getUrlFoto())
+                .into(holder.imgAvatar);
+        // Se establece el estado de selección del elemento.
+        holder.itemView.setActivated(mSelectedItems.get(position, false));
+    }
+
+    // Retorna el número de ítems gestionados.
+    @Override
+    public int getItemCount() {
+        return mDatos.size();
+    }
+
+    // Retorna un objeto de la colección.
+    public Alumno getItem(int position) {
+        return mDatos.get(position);
+    }
+
+
+    // Elimina un elemento de la lista.
+    public void removeItem(int position) {
+        mDatos.remove(position);
+        notifyItemRemoved(position);
+        // Se comprueba si pasa a estar vacía.
+        checkEmptyStateChanged();
+    }
+
+    // Elimina los elementos seleccionados.
+    public void removeSelectedItems() {
+        // Se eliminan en orden inverso para que no haya problemas. Al
+        // eliminar se cambia el
+        // estado de selección del elemento.
+        List<Integer> seleccionados = getSelectedItemsPositions();
+        Collections.sort(seleccionados, Collections.reverseOrder());
+        for (int i = 0; i < seleccionados.size(); i++) {
+            int pos = seleccionados.get(i);
+            toggleSelection(pos);
+            removeItem(pos);
+        }
+        // Se comprueba si pasa a estar vacía.
+        checkEmptyStateChanged();
+    }
+
+    // Añade un elemento a la lista.
+    public void addItem(Alumno alumno, int position) {
+        // Se añade el elemento.
+        mDatos.add(position, alumno);
+        // Se notifica que se ha insertado un elemento en la última posición.
+        notifyItemInserted(position);
+        // Si comprueba si deja de estar vacía.
+        checkEmptyStateChanged();
+    }
+
+    // Intercambia dos elementos de la lista.
+    void swapItems(int from, int to) {
+        // Se realiza el intercambio.
+        Collections.swap(mDatos, from, to);
+        // Se notifica el movimiento.
+        notifyItemChanged(from);
+        notifyItemChanged(to);
+    }
+
+    // Comprueba si ha pasa a estar vacía o deja de estar vacía.
+    private void checkEmptyStateChanged() {
+        // Deja de estar vacía
+        if (mIsEmpty && mDatos.size() > 0) {
+            mIsEmpty = false;
+            if (mOnEmptyStateChangedListener != null) {
+                mOnEmptyStateChangedListener.onEmptyStateChanged(false);
+            }
+        }
+        else if (!mIsEmpty && mDatos.size()==0) {
+            mIsEmpty = true;
+            if (mOnEmptyStateChangedListener != null) {
+                mOnEmptyStateChangedListener.onEmptyStateChanged(true);
+            }
+        }
+    }
+
+    // Establece el listener a informar cuando se hace click sobre un
+    // elemento de la lista.
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mOnItemClickListener = listener;
+    }
+
+    // Establece el listener a informar cuando se hace click largo sobre un
+    // elemento de la lista.
+    public void setOnItemLongClickListener(OnItemLongClickListener listener) {
+        mOnItemLongClickListener = listener;
+    }
+
+    // Establece el listener a informar cuando la lista pasa a o deja de
+    // estar vacía.
+    public void setOnEmptyStateChangedListener(
+            OnEmptyStateChangedListener listener) {
+        mOnEmptyStateChangedListener = listener;
+        checkEmptyStateChanged();
+    }
+
+    // Cambia el estado de selección de un elemento.
+    public void toggleSelection(int position) {
+        if (mSelectedItems.get(position, false)) {
+            mSelectedItems.delete(position);
+        } else {
+            mSelectedItems.put(position, true);
+        }
+        notifyItemChanged(position);
+    }
+
+    // Selecciona un elemento.
+    public void setSelected(int position) {
+        mSelectedItems.put(position, true);
+        notifyItemChanged(position);
+
+    }
+
+    // Quita la selección de un elemento.
+    public void clearSelection(int position) {
+        if (mSelectedItems.get(position, false)) {
+            mSelectedItems.delete(position);
+        }
+        notifyItemChanged(position);
+    }
+
+    // Quita la selección de todos los elementos seleccionados.
+    public void clearSelections() {
+        if (mSelectedItems.size() > 0) {
+            mSelectedItems.clear();
+            notifyDataSetChanged();
+        }
+    }
+
+    // Retorna el número de elementos seleccionados.
+    public int getSelectedItemCount() {
+        return mSelectedItems.size();
+    }
+
+    // Retorna un array con las posiciones de los elementos seleccionados.
+    public List<Integer> getSelectedItemsPositions() {
+        List<Integer> items = new ArrayList<>(mSelectedItems.size());
+        for (int i = 0; i < mSelectedItems.size(); i++) {
+            items.add(mSelectedItems.keyAt(i));
+        }
+        return items;
+    }
+
+    // Intercambia los elementos indicados.
+    public void moveItem(int start, int end) {
+        int max = Math.max(start, end);
+        int min = Math.min(start, end);
+        if (min >= 0 && max < mDatos.size()) {
+            Alumno item = mDatos.remove(min);
+            mDatos.add(max, item);
+            notifyItemMoved(min, max);
+        }
+    }
+
+    // Contenedor de vistas para la vista-fila.
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        // El contenedor de vistas para un elemento de la lista debe contener...
+        private final TextView lblNombre;
+        private final TextView lblDireccion;
+
+
+        private final CircleImageView imgAvatar;
+
+        // El constructor recibe la vista-fila.
+        public ViewHolder(View itemView) {
+            super(itemView);
+            // Se obtienen las vistas de la vista-fila.
+            lblNombre = (TextView) itemView.findViewById(R.id.lblNombre);
+            lblDireccion = (TextView) itemView.findViewById(R.id.lblDireccion);
+            imgAvatar = (CircleImageView) itemView.findViewById(R.id.imgAvatar);
+        }
+
+    }
+
+}
---
+++ PR137-ParallaxRecyclerView/app/src/main/java/es/iessaladillo/pedrojoya/pr137/MainActivity.java
@@ -0,0 +1,194 @@
+package es.iessaladillo.pedrojoya.pr137;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.software.shell.fab.ActionButton;
+
+
+public class MainActivity extends AppCompatActivity implements AlumnosAdapter.OnItemClickListener, AlumnosAdapter.OnItemLongClickListener, AlumnosAdapter.OnEmptyStateChangedListener {
+
+    private ImageView imgFoto;
+    private ScrollView svScroll;
+    private Toolbar toolbar;
+    private TextView lblTitulo;
+    private View vOverlay;
+    private RecyclerView lstAlumnos;
+    private AlumnosAdapter mAdaptador;
+    private ActionButton btnAgregar;
+    private TextView lblNoHayAlumnos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initVistas();
+    }
+
+    // Obtiene e inicializa las vistas.
+    private void initVistas() {
+        btnAgregar = (ActionButton) findViewById(R.id.btnAgregar);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                agregarAlumno(DB.getNextAlumno());
+            }
+        });
+        lblNoHayAlumnos = (TextView) findViewById(R.id.lblNoHayAlumnos);
+        lstAlumnos = (RecyclerView) findViewById(R.id.lstAlumnos);
+        lstAlumnos.setHasFixedSize(true);
+        mAdaptador = new AlumnosAdapter(DB.getAlumnos());
+        mAdaptador.setOnItemClickListener(this);
+        mAdaptador.setOnItemLongClickListener(this);
+        mAdaptador.setOnEmptyStateChangedListener(this);
+        lstAlumnos.setAdapter(mAdaptador);
+        lstAlumnos.setLayoutManager(
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        lstAlumnos.setItemAnimator(new DefaultItemAnimator());
+        imgFoto = (ImageView) findViewById(R.id.imgFoto);
+        lblTitulo = (TextView) findViewById(R.id.lblTitulo);
+        vOverlay = findViewById(R.id.vOverlay);
+        // Cuando se hace scroll se realiza el efecto parallax.
+        parallax(0, imgFoto);
+        lstAlumnos.addOnScrollListener(new HidingScrollListener() {
+            @Override
+            public void onHide() {
+                btnAgregar.hide();
+            }
+
+            @Override
+            public void onShow() {
+                btnAgregar.show();
+            }
+        });
+        lstAlumnos.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                parallax(recyclerView.computeVerticalScrollOffset(), imgFoto);
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
+    }
+
+    // Realiza el efecto parallax sobre la imagen a partir del scroll realizado.
+    private void parallax(int scrollY, ImageView image) {
+
+        // Datos básicos.
+        int imageHeight = getResources().getDimensionPixelSize(R.dimen.image_height);
+        // En el onCreate la action bar aún no tiene altura.
+        int actionBarHeight = Math.max(getSupportActionBar().getHeight(),
+                getResources().getDimensionPixelSize(R.dimen
+                        .abc_action_bar_default_height_material));
+        // En el onCreate lblTitulo aún no tiene altura.
+        int lblTituloHeight = Math.max(lblTitulo.getHeight(), actionBarHeight);
+        int baseColor = getResources().getColor(R.color.primary);
+        Log.d(getString(R.string.app_name), "ScrollY: " + scrollY);
+
+        // La imagen se traslada hacia arriba la mitad de la cantidad de scroll
+        // que hayamos realizado. El overlay se traslada con la imagen.
+        image.setTranslationY(-scrollY / 2);
+        vOverlay.setTranslationY(-scrollY / 2);
+        Log.d(getString(R.string.app_name), "Traslación imagen: " +
+                -scrollY);
+
+        // La toolbar se vuelve más opaca en relación a la cantidad de scroll.
+        // Como mxáximo será 1 (completamente opaca) aunque hayamos más scroll
+        // que la altura de la imagen.
+        float alpha = Math.min(1, (float) scrollY / imageHeight);
+        int alphaColor = getColorWithAlpha(alpha, baseColor);
+        //toolbar.setBackgroundColor(alphaColor);
+
+        // Escalado del título. Cuanto más scroll más pequeño será la
+        // escala. Como mínimo 1. Como máximo 1,3.
+        float flexibleRange = imageHeight - actionBarHeight;
+        float scale = 1 + ((flexibleRange - scrollY) / flexibleRange);
+        scale = Math.max(1, scale);
+        scale = Math.min(1.3f, scale);
+        lblTitulo.setPivotX(0);
+        lblTitulo.setPivotY(0);
+        lblTitulo.setScaleX(scale);
+        lblTitulo.setScaleY(scale);
+        Log.d(getString(R.string.app_name), "Escala: " + scale);
+
+        // El overlay se vuelve más opaca conforme se hace scroll.
+        float maxOverlayDistance = imageHeight - actionBarHeight + (lblTituloHeight * scale);
+        float overlayAlpha = Math.min(maxOverlayDistance, scrollY)
+                / maxOverlayDistance;
+        vOverlay.setAlpha(overlayAlpha);
+        Log.d(getString(R.string.app_name), "Alpha overlay: " + overlayAlpha);
+
+        // Translación hacia abajo del título. Caunto más scroll menos
+        // traslación. Como máximo la altura de la imagen menos
+        // la altura del TextView (en la escala actual).
+        // Como minimo 0 (situándose debajo de la toolbar).
+        int maxTitleTranslationY = (int) (imageHeight - lblTituloHeight *
+                scale);
+        int titleTranslationY = maxTitleTranslationY - scrollY;
+        titleTranslationY = Math.max(0, titleTranslationY);
+        lblTitulo.setTranslationY(titleTranslationY);
+
+        // El título de la toolbar sólo debe mostrarse si el título se ha
+        // trasladado por completo.
+        if (titleTranslationY == 0) {
+            getSupportActionBar().setTitle(getString(R.string.app_name));
+            toolbar.setBackgroundColor(getResources().getColor(R.color.primary));
+        } else {
+            getSupportActionBar().setTitle("");
+            toolbar.setBackgroundColor(getResources().getColor(android.R
+                    .color.transparent));
+        }
+        Log.d(getString(R.string.app_name), "Traslación título: " + titleTranslationY);
+    }
+
+    // Retorna el color base aplicandole una determinada transaparencia alpha.
+    public int getColorWithAlpha(float alpha, int baseColor) {
+        int a = Math.min(255, Math.max(0, (int) (alpha * 255))) << 24;
+        int rgb = 0x00ffffff & baseColor;
+        return a + rgb;
+    }
+
+    // Cuando se hace click sobre un elemento de la lista.
+    @Override
+    public void onItemClick(View view, Alumno alumno, int position) {
+        Toast.makeText(this, getString(R.string.ha_pulsado_sobre) + " " + alumno.getNombre(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    // Cuando se hace long click sobre un elemento de la lista.
+    @Override
+    public void onItemLongClick(View view, Alumno alumno, int position) {
+        // Se elimina el alumno.
+        mAdaptador.removeItem(position);
+
+    }
+
+    // Agrega un alumno a la lista.
+    private void agregarAlumno(Alumno alumno) {
+        // Se agrega el alumno.
+        mAdaptador.addItem(alumno, mAdaptador.getItemCount());
+        lstAlumnos.scrollToPosition(mAdaptador.getItemCount() - 1);
+    }
+
+    @Override
+    public void onEmptyStateChanged(boolean isEmpty) {
+        lblNoHayAlumnos.setVisibility(isEmpty?View.VISIBLE:View.INVISIBLE);
+    }
+}
