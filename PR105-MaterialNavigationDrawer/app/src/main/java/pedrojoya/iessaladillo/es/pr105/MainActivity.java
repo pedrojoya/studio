5521f3f1bdb867b505007ba5fb8cef83156ed5d0
pedrojoya <pedrojoya@gmail.com>
2015-06-03 10:07:34 +0200

    PR105 con fragmentos

--- PR105-MaterialNavigationDrawer/app/src/main/java/pedrojoya/iessaladillo/es/pr105/MainActivity.java
+++ PR105-MaterialNavigationDrawer/app/src/main/java/pedrojoya/iessaladillo/es/pr105/MainActivity.java
@@ -1,151 +1,123 @@
 package pedrojoya.iessaladillo.es.pr105;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    // Fragmento de navegación.
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    // Último título de la toolbar mostrado en pantalla. Se usa en {@link #restoreActionBar()}.
-    private CharSequence mTitle;
+    private static final String STATE_SELECTED_NAVITEM_ID = "selectedNavItemPosition";
+    private static final String PREFERENCES_FILE = "prefs";
+    private static final String PREF_NAV_DRAWER_OPENED = "navdrawerOpened";
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private int mSelectedNavItemId = -1;
+    private CircleImageView mImgProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Se establece la toolbar como action bar.
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // La action bar debe mostrar la hamburguesa.
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
-
-
-        // Se obtiene el fragmento de navegación.
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-
-        // Se guarda el título actual de la actividad.
-        mTitle = getTitle();
-
-        // Se configura el drawer.
-        mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
-    }
-
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
+        initVistas();
+        if (savedInstanceState != null) {
+            mSelectedNavItemId =
+                    savedInstanceState.getInt(STATE_SELECTED_NAVITEM_ID);
+            MenuItem item = mNavigationView.getMenu().findItem(mSelectedNavItemId);
+            item.setChecked(true);
+            setTitle(item.getTitle());
+        } else {
+            onNavigationItemSelected(mNavigationView.getMenu().getItem(0));
         }
     }
 
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STATE_SELECTED_NAVITEM_ID, mSelectedNavItemId);
     }
 
+    // Obtiene e inicializa las vistas.
+    private void initVistas() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        configToolbar();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mImgProfile = (CircleImageView) findViewById(R.id.imgProfile);
+        configNavigationDrawer();
+    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
+    // Configura la toolbar.
+    private void configToolbar() {
+        // Se establece la toolbar como action bar y se obliga a mostrar
+        // el icono para abrir el naviagation drawer.
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
         }
-        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Configura el navigation drawer.
+    private void configNavigationDrawer() {
+        Picasso.with(this).load("http://lorempixel.com/200/200/people/").into(mImgProfile);
+        mNavigationView.setNavigationItemSelectedListener(this);
+        if (!leerPreferencia()) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            guardarPreferencia();
+        }
+    }
+
+    private void cargarFragmento(String title) {
+        setTitle(title);
+        MainFragment frg = MainFragment.newInstance(title);
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, frg, title).commit();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == android.R.id.home) {
+            // Se abre el panel.
+            mDrawerLayout.openDrawer(GravityCompat.START);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        mSelectedNavItemId = menuItem.getItemId();
+        // Se carga el fragmento correspondiente.
+        cargarFragmento(menuItem.getTitle().toString());
+        // Se selecciona el elemento.
+        menuItem.setChecked(true);
+        // Se cierra el navigation drawer.
+        mDrawerLayout.closeDrawers();
+        return true;
+    }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
+    private void guardarPreferencia() {
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(PREF_NAV_DRAWER_OPENED, true);
+        editor.apply();
+    }
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
+    public boolean leerPreferencia() {
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        return sharedPref.getBoolean(PREF_NAV_DRAWER_OPENED, false);
     }
 
 }
---
+++ PR105-MaterialNavigationDrawer/app/src/main/java/pedrojoya/iessaladillo/es/pr105/MainFragment.java
@@ -0,0 +1,56 @@
+package pedrojoya.iessaladillo.es.pr105;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MainFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "opcion";
+
+    private String mOpcion;
+    private TextView lblTexto;
+
+    public static MainFragment newInstance(String param1) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MainFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mOpcion = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getView() != null) {
+            initVistas(getView());
+        }
+    }
+
+    private void initVistas(View view) {
+        lblTexto = (TextView) view.findViewById(R.id.lblTexto);
+        //lblTexto.setText(mOpcion);
+    }
+}
