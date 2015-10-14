197c20a59e85c31459ce0b0ec0c49c3dfac8bc19
pedrojoya <pedrojoya@gmail.com>
2015-10-14 09:07:26 +0200

    Fix PR128

--- PR128-OverridePendingTransition/app/src/main/java/es/iessaladillo/pedrojoya/pr128/MainActivity.java
+++ PR128-OverridePendingTransition/app/src/main/java/es/iessaladillo/pedrojoya/pr128/MainActivity.java
@@ -1,24 +1,46 @@
 package es.iessaladillo.pedrojoya.pr128;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private static final int ANIM_PULL_RIGHT_INDEX = 1;
+
+    private Spinner spnSalida;
+    private Spinner spnEntrada;
+
+    private final int[] mResIdsAnimacionesSalida =
+            {R.anim.push_right, R.anim.push_left, R.anim.push_up,
+                    R.anim.push_down, R.anim.scale_down_disappear};
+    private final int[] mResIdsAnimacionesEntrada =
+            {R.anim.pull_left, R.anim.pull_right, R.anim.pull_down,
+                    R.anim.pull_up, R.anim.scale_up_appear};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((RelativeLayout) findViewById(R.id.rlRaiz)).setOnClickListener(new View.OnClickListener() {
+        spnSalida = (Spinner) findViewById(R.id.spnSalida);
+        spnEntrada = (Spinner) findViewById(R.id.spnEntrada);
+        // Se selecciona inicialmente la animación de entrada que concuerda
+        // con la animación de salida seleccionada por defecto.
+        spnEntrada.setSelection(ANIM_PULL_RIGHT_INDEX);
+        findViewById(R.id.btnIr).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), OtraActivity.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.left_in, R.anim.right_out);
+                // Se lanza la otra actividad (se le envían las animaciones que
+                // deben llevarse a cabo en el retorno.
+                OtraActivity.start(MainActivity.this,
+                        mResIdsAnimacionesSalida[spnEntrada.getSelectedItemPosition()],
+                        mResIdsAnimacionesEntrada[spnSalida.getSelectedItemPosition()]);
+                // Se realiza la animación (entrada, salida).
+                overridePendingTransition(
+                        mResIdsAnimacionesEntrada[spnEntrada.getSelectedItemPosition()],
+                        mResIdsAnimacionesSalida[spnSalida.getSelectedItemPosition()]);
             }
         });
     }
--- PR128-OverridePendingTransition/app/src/main/java/es/iessaladillo/pedrojoya/pr128/OtraActivity.java
+++ PR128-OverridePendingTransition/app/src/main/java/es/iessaladillo/pedrojoya/pr128/OtraActivity.java
@@ -1,17 +1,28 @@
 package es.iessaladillo.pedrojoya.pr128;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 
-public class OtraActivity extends ActionBarActivity {
+public class OtraActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANIM_RETORNO = "extra_anim_retorno";
+    private static final String EXTRA_ANIM_REENTRADA = "extra_anim_reentrada";
+
+    private int mResIdAnimRetorno;
+    private int mResIdAnimReentrada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otra);
+        // Se obtienen del intent los extras recibidos.
+        mResIdAnimRetorno = getIntent().getIntExtra(EXTRA_ANIM_RETORNO, 0);
+        mResIdAnimReentrada = getIntent().getIntExtra(EXTRA_ANIM_REENTRADA, 0);
     }
 
 
@@ -19,9 +30,27 @@ public class OtraActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
+            // Se navega de vuelta a la actividad llamadora.
             NavUtils.navigateUpFromSameTask(this);
-            overridePendingTransition(R.anim.right_in, R.anim.left_out);
+            // Se aplica la animación (entrada, salida).
+            overridePendingTransition(mResIdAnimReentrada, mResIdAnimRetorno);
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        // Se aplica la animación (entrada, salida).
+        overridePendingTransition(mResIdAnimReentrada, mResIdAnimRetorno);
+    }
+
+    // Método estático para lanzar esta actividad.
+    public static void start(Context context, int resIdAnimRetorno, int resIdAnimReentrada) {
+        Intent intent = new Intent(context, OtraActivity.class);
+        intent.putExtra(EXTRA_ANIM_RETORNO, resIdAnimRetorno);
+        intent.putExtra(EXTRA_ANIM_REENTRADA, resIdAnimReentrada);
+        context.startActivity(intent);
+    }
+
 }
