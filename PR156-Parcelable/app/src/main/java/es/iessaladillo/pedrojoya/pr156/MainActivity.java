b688d4151eb74315b0befd00ed729f3a97c2f18b
pedrojoya <pedrojoya@gmail.com>
2015-10-17 14:30:56 +0200

    PR156 Add

---
+++ PR156-Parcelable/app/src/main/java/es/iessaladillo/pedrojoya/pr156/MainActivity.java
@@ -0,0 +1,90 @@
+package es.iessaladillo.pedrojoya.pr156;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements
+        OnClickListener {
+
+    // Constantes.
+    private static final int RC_ALUMNO = 1;
+
+    // Variables a nivel de clase.
+    private Alumno mAlumno;
+
+    private TextView lblDatos;
+
+    // Al crear la actividad.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Se crea el alumno.
+        mAlumno = new Alumno("", Alumno.DEFAULT_EDAD);
+        // Se obtienen e inicializan las vistas.
+        initVistas();
+    }
+
+    // Obtiene e inicializa las vistas.
+    private void initVistas() {
+        Button btnSolicitar = (Button) this.findViewById(R.id.btnSolicitar);
+        // La actividad responderá al pulsar el botón.
+        btnSolicitar.setOnClickListener(this);
+        lblDatos = (TextView) this.findViewById(R.id.lblDatos);
+    }
+
+    // Cuando se hace click.
+    public void onClick(View vista) {
+        // Dependiendo del botón.
+        switch (vista.getId()) {
+            case R.id.btnSolicitar:
+                // Solicito los datos del alumno.
+                solicitarDatos();
+                break;
+        }
+    }
+
+    // Muestra la actividad AlumnoActivity en espera de respuesta.
+    private void solicitarDatos() {
+        AlumnoActivity.startForResult(this, RC_ALUMNO, mAlumno);
+    }
+
+    // Cuando llega una respuesta.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Si el resultado es satisfactorio.
+        if (resultCode == RESULT_OK) {
+            // Depeniendo del código de petición (Request Code)
+            switch (requestCode) {
+                case RC_ALUMNO:
+                    // Se obtienen los datos de retorno.
+                    getDatosRetorno(data);
+            }
+        }
+    }
+
+    // Obtiene los datos retornados del intent de retorno.
+    private void getDatosRetorno(Intent intentRetorno) {
+        // Se actualiza el alumno en base a los datos recibidos.
+        if (intentRetorno != null) {
+            if (intentRetorno.hasExtra(AlumnoActivity.EXTRA_ALUMNO)) {
+                mAlumno = intentRetorno
+                        .getParcelableExtra(AlumnoActivity.EXTRA_ALUMNO);
+            }
+        }
+        // Se muestran los datos del alumno.
+        alumnoToViews();
+    }
+
+    // Muestra los datos del alumno en las vistas.
+    private void alumnoToViews() {
+        lblDatos.setText(getString(R.string.datos, mAlumno.getNombre(),
+                mAlumno.getEdad()));
+    }
+
+}
