e6895807d8ac426a28188d93496b2c47b76ebb6c
pedrojoya <pedrojoya@gmail.com>
2016-06-15 08:11:58 +0200

    PR183 Go on

--- PR183-FirebasePush/app/src/main/java/es/iessaldillo/pedrojoya/pr183/ServidorContrat.java
+++ PR183-FirebasePush/app/src/main/java/es/iessaldillo/pedrojoya/pr183/ServidorContrat.java
@@ -6,6 +6,6 @@ public class ServidorContrat {
         public static String TOKEN = "Token";
     }
 
-    public static String URL_SERVIDOR = "www.informaticasaladillo.es/android/fcm";
-    public static String URL_REGISTRO = URL_SERVIDOR + "/registrartoken.php";
+    public static String URL_REGISTRO = "http://www.informaticasaladillo.es/android/fcm/registrartoken.php";
+    public static String URL_ENVIAR = "http://www.informaticasaladillo.es/android/fcm/enviarmensaje.php";
 }
