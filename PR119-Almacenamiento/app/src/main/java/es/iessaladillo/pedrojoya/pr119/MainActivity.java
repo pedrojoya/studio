4fc965ca52156821465f1234672b695c3a2ec991
pedrojoya <pedrojoya@gmail.com>
2016-09-14 00:27:26 +0200

    PR119 Fix error on orientation change

--- PR119-Almacenamiento/app/src/main/java/es/iessaladillo/pedrojoya/pr119/MainActivity.java
+++ PR119-Almacenamiento/app/src/main/java/es/iessaladillo/pedrojoya/pr119/MainActivity.java
@@ -1,15 +1,19 @@
 package es.iessaladillo.pedrojoya.pr119;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -24,8 +28,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import butterknife.ButterKnife;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 @SuppressWarnings({"WeakerAccess", "unused"})
@@ -49,79 +53,80 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.rbCacheExterno)
     RadioButton mRbCacheExterno;
 
-    File mCarpetaDestino;
-    InputStream mEntrada;
-    String mNombreArchivo;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        habilitarOpciones(
+                Environment.getExternalStorageState().equals(
+                        Environment.MEDIA_MOUNTED));
+    }
+
+    private void habilitarOpciones(boolean mounted) {
+        mRbExternoPropio.setEnabled(mounted);
+        mRbExternoPublico.setEnabled(mounted);
+        mRbCacheExterno.setEnabled(mounted);
     }
 
     @OnClick(R.id.btnCopiar)
     public void btnCopiarOnClick() {
-        // Se selecciona el tipo de subcarpeta dependiendo del origen.
-        String tipoSubcarpeta =
-                mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ?
-                        Environment.DIRECTORY_DOWNLOADS :
-                        Environment.DIRECTORY_MUSIC;
-        // Se obtiene la carpeta de destino.
-        boolean permisoRequerido = false;
+        // Si no tenemos el permiso, lo solicitamos.
+        if (isPermisoRequerido() && !puedeEscribirEnAlmacenamientoExterno()) {
+            solicitarPermisoEscrituraAlmacenamientoExterno();
+        } else {
+            // Se copia el archivo en la carpeta de destino.
+            copiarArchivo();
+        }
+    }
+
+    private String getTipoSubcarpeta() {
+        return mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ?
+                Environment.DIRECTORY_DOWNLOADS :
+                Environment.DIRECTORY_MUSIC;
+    }
+
+    private File getCarpetaDestino() {
         switch (mRgDestino.getCheckedRadioButtonId()) {
-            case R.id.rbInterno:
-                mCarpetaDestino = getFilesDir();
-                break;
             case R.id.rbExternoPropio:
-                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                    mCarpetaDestino = getExternalFilesDir(tipoSubcarpeta);
-                    permisoRequerido = true;
-                }
-                break;
+                return getExternalFilesDir(getTipoSubcarpeta());
             case R.id.rbExternoPublico:
-                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                    mCarpetaDestino = Environment.getExternalStoragePublicDirectory(tipoSubcarpeta);
-                    permisoRequerido = true;
-                }
-                break;
+                return Environment.getExternalStoragePublicDirectory(
+                        getTipoSubcarpeta());
             case R.id.rbCacheInterno:
-                mCarpetaDestino = getCacheDir();
-                break;
+                return getCacheDir();
             case R.id.rbCacheExterno:
-                mCarpetaDestino = getExternalCacheDir();
-                permisoRequerido = true;
-                break;
+                return getExternalCacheDir();
+            default:
+                return getFilesDir();
         }
+    }
 
-        // Si es posible realizar la copia.
-        if (mCarpetaDestino != null) {
-            try {
-                // Se abre al flujo de entrada correspondiente.
-                if (mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw) {
-                    mEntrada = getResources().openRawResource(R.raw.lorem);
-                    mNombreArchivo = RAW_FILE_NAME;
-                } else {
-                    mEntrada = getAssets().open(ASSET_FILE_NAME);
-                    mNombreArchivo = ASSET_FILE_NAME;
-                }
-                // Si no tenemos el permiso, lo solicitamos.
-                if (permisoRequerido && puedeEscribirEnAlmacenamientoExterno()) {
-                    solicitarPermisoEscrituraAlmacenamientoExterno();
-                } else {
-                    // Se copia el archivo en la carpeta de destino.
-                    copiarArchivo();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    private InputStream getFlujoEntrada() throws IOException {
+        return mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ?
+                getResources().openRawResource(R.raw.lorem) :
+                getAssets().open(getNombreArchivo());
+    }
 
+    private String getNombreArchivo() {
+        return mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ?
+                RAW_FILE_NAME : ASSET_FILE_NAME;
+    }
+
+    @NonNull
+    private String getMimeType() {
+        return mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ?
+                "text/plain" : "audio/mp3";
+    }
+
+    private boolean isPermisoRequerido() {
+        int id = mRgDestino.getCheckedRadioButtonId();
+        return (id == R.id.rbExternoPropio) || (id == R.id.rbExternoPublico) ||
+                (id == R.id.rbCacheExterno);
     }
 
     private boolean tienePermiso(String permissionName) {
-        return ContextCompat.checkSelfPermission(this, permissionName) !=
+        return ContextCompat.checkSelfPermission(this, permissionName) ==
                 PackageManager.PERMISSION_GRANTED;
     }
 
@@ -130,55 +135,89 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void solicitarPermisoEscrituraAlmacenamientoExterno() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                RP_ALMACEN_EXTERNO);
+        // Comprobamos si hay que mostrar un diálogo informativo.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            mostrarDialogoInformativoPermiso();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    RP_ALMACEN_EXTERNO);
+        }
+    }
+
+    private void mostrarDialogoInformativoPermiso() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.es_necesario)
+                .setTitle(R.string.permiso_requerido)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        RP_ALMACEN_EXTERNO);
+                            }
+                        })
+                .show();
+    }
+
+    public static void startInstalledAppDetailsActivity(
+            @NonNull final Activity context) {
+        final Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setData(Uri.parse("package:" + context.getPackageName()));
+        // Para que deje rastro en la pila de actividades se añaden flags.
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        context.startActivity(intent);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case RP_ALMACEN_EXTERNO: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Se copia el archivo en la carpeta de destino.
-                    copiarArchivo();
-                }
-                else {
-                    // Comprobamos si el usuario ha marcado No volver a preguntar.
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                        // Aún tenemos esperanza. Informamos.
-                        Snackbar.make(mBtnCopiar,
-                                "Imposible realizar la acción si no se concede el permiso", Snackbar.LENGTH_LONG)
-                                .show();
-                    }
-                    else {
-                        // El usuario ha indicado que No se le vuelva a preguntar.
-                        Snackbar.make(mBtnCopiar,
-                                "Funciones deshabilitadas al no obtener permiso", Snackbar.LENGTH_LONG)
-                                .show();
-                        deshabilitarOpcionesExterno();
-                    }
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        if (requestCode == RP_ALMACEN_EXTERNO) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Se copia el archivo en la carpeta de destino.
+                copiarArchivo();
+            } else {
+                // Comprobamos si el usuario ha marcado No volver a preguntar.
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    // Aún tenemos esperanza. Informamos.
+                    Snackbar.make(mBtnCopiar,
+                            R.string.no_se_pudo, Snackbar.LENGTH_LONG)
+                            .show();
+                } else {
+                    // El usuario ha indicado que No se le vuelva a preguntar.
+                    Snackbar.make(mBtnCopiar,
+                            R.string.accion_no_disponible, Snackbar.LENGTH_LONG)
+                            .setAction(R.string.configurar,
+                                    new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            startInstalledAppDetailsActivity(
+                                                    MainActivity.this);
+                                        }
+                                    })
+                            .show();
                 }
             }
         }
     }
 
-    private void deshabilitarOpcionesExterno() {
-        mRbExternoPropio.setEnabled(false);
-        mRbExternoPublico.setEnabled(false);
-        mRbCacheExterno.setEnabled(false);
-    }
-
-    // Copia el archivo representado por el flujo de entrada ya abierto en la carpeta
+    // Copia el archivo representado por el flujo de entrada en la carpeta
     // de destino y con el nombre de destino indicado.
     private void copiarArchivo() {
-        BufferedInputStream lector = new BufferedInputStream(mEntrada);
-        final File fichero = new File(mCarpetaDestino, mNombreArchivo);
-        Log.d(getString(R.string.app_name), fichero.getPath());
+        BufferedInputStream lector;
         try {
+            lector = new BufferedInputStream(getFlujoEntrada());
+            final File fichero = new File(getCarpetaDestino(), getNombreArchivo());
+            Log.d(getString(R.string.app_name), fichero.getPath());
             FileOutputStream salida = new FileOutputStream(fichero);
             BufferedOutputStream escritor = new BufferedOutputStream(salida);
             byte[] array = new byte[BUFFER_SIZE];
@@ -190,14 +229,15 @@ public class MainActivity extends AppCompatActivity {
             escritor.close();
             lector.close();
             Snackbar.make(mBtnCopiar,
-                    getString(R.string.generado, fichero.getPath()), Snackbar.LENGTH_LONG)
+                    getString(R.string.generado, fichero.getPath()),
+                    Snackbar.LENGTH_LONG)
                     .setAction(R.string.abrir, new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             abrirArchivo(fichero);
                         }
                     }).show();
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -208,9 +248,7 @@ public class MainActivity extends AppCompatActivity {
         // accedido desde una aplicación externa.
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
-        String mimeType = mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ?
-                "text/plain" : "audio/mp3";
-        intent.setDataAndType(Uri.fromFile(fichero), mimeType);
+        intent.setDataAndType(Uri.fromFile(fichero), getMimeType());
         try {
             startActivity(intent);
         } catch (Exception e) {
