4252644f82db29f5dd820327c826d3c993c45f62
pedrojoya <pedrojoya@gmail.com>
2017-09-08 18:28:21 +0200

    Upgrade version

--- PR188-PermissionsDispatcher/app/src/main/java/es/iessaladillo/pedrojoya/pr188/MainActivity.java
+++ PR188-PermissionsDispatcher/app/src/main/java/es/iessaladillo/pedrojoya/pr188/MainActivity.java
@@ -1,12 +1,9 @@
 package es.iessaladillo.pedrojoya.pr188;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -16,17 +13,14 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import es.iessaladillo.pedrojoya.pr188.utils.FileUtils;
+import es.iessaladillo.pedrojoya.pr188.utils.IntentUtils;
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.OnNeverAskAgain;
 import permissions.dispatcher.OnPermissionDenied;
@@ -34,36 +28,40 @@ import permissions.dispatcher.OnShowRationale;
 import permissions.dispatcher.PermissionRequest;
 import permissions.dispatcher.RuntimePermissions;
 
-@SuppressWarnings({"WeakerAccess", "unused", "CanBeFinal"})
 @RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
     private static final String RAW_FILE_NAME = "lorem.txt";
     private static final String ASSET_FILE_NAME = "audio.mp3";
-    private static final int BUFFER_SIZE = 1000;
-    private static final int RP_ALMACEN_EXTERNO = 1;
+    private static final int RP_WRITE_EXTERNAL = 1;
 
-    @BindView(R.id.rgOrigen)
-    RadioGroup mRgOrigen;
-    @BindView(R.id.rgDestino)
-    RadioGroup mRgDestino;
-    @BindView(R.id.btnCopiar)
-    Button mBtnCopiar;
-    @BindView(R.id.rbExternoPropio)
-    RadioButton mRbExternoPropio;
-    @BindView(R.id.rbExternoPublico)
-    RadioButton mRbExternoPublico;
-    @BindView(R.id.rbCacheExterno)
-    RadioButton mRbCacheExterno;
+    private RadioGroup rgSource;
+    private RadioGroup rgDestination;
+    private Button btnDuplicate;
+    private RadioButton rbPersonalExternal;
+    private RadioButton rbPublicExternal;
+    private RadioButton rbExternalCache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        habilitarOpciones(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED));
+        initViews();
+        enableOptions(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED));
     }
 
+    private void initViews() {
+        rgSource = findViewById(R.id.rgSource);
+        rgDestination = findViewById(R.id.rgDestination);
+        btnDuplicate = findViewById(R.id.btnDuplicate);
+        rbPersonalExternal = findViewById(R.id.rbPersonalExternal);
+        rbPublicExternal = findViewById(R.id.rbPublicExternal);
+        rbExternalCache = findViewById(R.id.rbExternalCache);
+
+        btnDuplicate.setOnClickListener(v -> btnDuplicateOnClick());
+    }
+
+    @SuppressLint("NeedOnRequestPermissionsResult")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
             @NonNull int[] grantResults) {
@@ -71,142 +69,112 @@ public class MainActivity extends AppCompatActivity {
                 grantResults);
     }
 
-    @OnClick(R.id.btnCopiar)
-    public void btnCopiarOnClick() {
-        // Si no tenemos el permiso, lo solicitamos.
-        if (isPermisoRequerido()) {
-            MainActivityPermissionsDispatcher.copiarArchivoConPermisoWithCheck(this);
+    private void btnDuplicateOnClick() {
+        if (hasRequiredPermission()) {
+            MainActivityPermissionsDispatcher.duplicateFileWithPermissionWithCheck(this);
         } else {
-            // Se copia el archivo en la carpeta de destino.
-            copiarArchivo();
+            duplicateFile();
         }
     }
 
     @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    void copiarArchivoConPermiso() {
-        copiarArchivo();
+    void duplicateFileWithPermission() {
+        duplicateFile();
     }
 
+    private void duplicateFile() {
+        try {
+            File outputFile = new File(getDestinyFolder(), getSourceFilename());
+            FileUtils.copyFile(getSourceInputStream(), outputFile);
+            Log.d(getString(R.string.app_name), outputFile.getPath());
+            Snackbar.make(btnDuplicate,
+                    getString(R.string.main_activity_file_duplicated, outputFile.getPath()),
+                    Snackbar.LENGTH_LONG).setAction(R.string.main_activity_open,
+                    v -> showFile(outputFile, getSourceMimeType())).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unused")
     @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    void mostrarDialogoInformativoPermiso(PermissionRequest request) {
-        new AlertDialog.Builder(this).setMessage(R.string.es_necesario).setTitle(
-                R.string.permiso_requerido).setPositiveButton(android.R.string.ok,
+    void showRationalePermissionDialog(PermissionRequest request) {
+        new AlertDialog.Builder(this).setMessage(
+                R.string.main_activity_permission_required_explanation).setTitle(
+                R.string.main_activity_permission_required).setPositiveButton(android.R.string.ok,
                 (dialogInterface, i) -> ActivityCompat.requestPermissions(MainActivity.this,
                         new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        RP_ALMACEN_EXTERNO)).show();
+                        RP_WRITE_EXTERNAL)).show();
     }
 
     @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    void mostrarError() {
-        Snackbar.make(mBtnCopiar, R.string.no_se_pudo, Snackbar.LENGTH_LONG).show();
+    void showError() {
+        Snackbar.make(btnDuplicate, R.string.main_activity_error_permission_required,
+                Snackbar.LENGTH_LONG).show();
     }
 
     @OnNeverAskAgain(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    void mostrarNoDisponible() {
-        Snackbar.make(mBtnCopiar, R.string.accion_no_disponible, Snackbar.LENGTH_LONG)
-                .setAction(R.string.configurar,
-                        view -> startInstalledAppDetailsActivity(MainActivity.this))
-                .show();
+    void showNotAvailable() {
+        Snackbar.make(btnDuplicate, R.string.main_activity_action_permission_required,
+                Snackbar.LENGTH_LONG).setAction(R.string.main_activity_configure,
+                view -> IntentUtils.startInstalledAppDetailsActivity(MainActivity.this)).show();
     }
 
-    private String getTipoSubcarpeta() {
-        return mRgOrigen.getCheckedRadioButtonId()
+    private String getSubfolderType() {
+        return rgSource.getCheckedRadioButtonId()
                        == R.id.rbRaw ? Environment.DIRECTORY_DOWNLOADS : Environment
                        .DIRECTORY_MUSIC;
     }
 
-    private File getCarpetaDestino() {
-        switch (mRgDestino.getCheckedRadioButtonId()) {
-            case R.id.rbExternoPropio:
-                return getExternalFilesDir(getTipoSubcarpeta());
-            case R.id.rbExternoPublico:
-                return Environment.getExternalStoragePublicDirectory(getTipoSubcarpeta());
-            case R.id.rbCacheInterno:
+    private File getDestinyFolder() {
+        switch (rgDestination.getCheckedRadioButtonId()) {
+            case R.id.rbPersonalExternal:
+                return getExternalFilesDir(getSubfolderType());
+            case R.id.rbPublicExternal:
+                return Environment.getExternalStoragePublicDirectory(getSubfolderType());
+            case R.id.rbInternalCache:
                 return getCacheDir();
-            case R.id.rbCacheExterno:
+            case R.id.rbExternalCache:
                 return getExternalCacheDir();
             default:
                 return getFilesDir();
         }
     }
 
-    private InputStream getFlujoEntrada() throws IOException {
-        return mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ? getResources().openRawResource(
-                R.raw.lorem) : getAssets().open(getNombreArchivo());
+    private InputStream getSourceInputStream() throws IOException {
+        return rgSource.getCheckedRadioButtonId() == R.id.rbRaw ? getResources().openRawResource(
+                R.raw.lorem) : getAssets().open(getSourceFilename());
     }
 
-    private String getNombreArchivo() {
-        return mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ? RAW_FILE_NAME : ASSET_FILE_NAME;
+    private String getSourceFilename() {
+        return rgSource.getCheckedRadioButtonId() == R.id.rbRaw ? RAW_FILE_NAME : ASSET_FILE_NAME;
     }
 
     @NonNull
-    private String getMimeType() {
-        return mRgOrigen.getCheckedRadioButtonId() == R.id.rbRaw ? "text/plain" : "audio/mp3";
+    private String getSourceMimeType() {
+        return rgSource.getCheckedRadioButtonId() == R.id.rbRaw ? "text/plain" : "audio/mp3";
     }
 
-    private boolean isPermisoRequerido() {
-        int id = mRgDestino.getCheckedRadioButtonId();
-        return (id == R.id.rbExternoPropio) || (id == R.id.rbExternoPublico) || (id
-                == R.id.rbCacheExterno);
+    private boolean hasRequiredPermission() {
+        int id = rgDestination.getCheckedRadioButtonId();
+        return (id == R.id.rbPersonalExternal) || (id == R.id.rbPublicExternal) || (id
+                == R.id.rbExternalCache);
     }
 
-    public static void startInstalledAppDetailsActivity(@NonNull final Activity context) {
-        final Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.addCategory(Intent.CATEGORY_DEFAULT);
-        intent.setData(Uri.parse("package:" + context.getPackageName()));
-        // Para que deje rastro en la pila de actividades se añaden flags.
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        context.startActivity(intent);
-    }
-
-    // Copia el archivo representado por el flujo de entrada en la carpeta
-    // de destino y con el nombre de destino indicado.
-    private void copiarArchivo() {
-        BufferedInputStream lector;
+    private void showFile(File file, String mimeType) {
+        // Files stored in personal directories can't be open from external apps.
         try {
-            lector = new BufferedInputStream(getFlujoEntrada());
-            final File fichero = new File(getCarpetaDestino(), getNombreArchivo());
-            Log.d(getString(R.string.app_name), fichero.getPath());
-            FileOutputStream salida = new FileOutputStream(fichero);
-            BufferedOutputStream escritor = new BufferedOutputStream(salida);
-            byte[] array = new byte[BUFFER_SIZE];
-            int leidos = lector.read(array);
-            while (leidos > 0) {
-                escritor.write(array, 0, leidos);
-                leidos = lector.read(array);
-            }
-            escritor.close();
-            lector.close();
-            Snackbar.make(mBtnCopiar, getString(R.string.generado, fichero.getPath()),
-                    Snackbar.LENGTH_LONG)
-                    .setAction(R.string.abrir, v -> abrirArchivo(fichero))
-                    .show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Envía un intent implícito para abrir el fichero.
-    private void abrirArchivo(File fichero) {
-        // Si se ha almacenado en directorio privado no podrá ser
-        // accedido desde una aplicación externa.
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(fichero), getMimeType());
-        try {
-            startActivity(intent);
+            startActivity(IntentUtils.newViewFileIntent(file, mimeType));
         } catch (Exception e) {
-            e.printStackTrace();
+            Toast.makeText(this, getString(R.string.main_activity_error_opening_file),
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void habilitarOpciones(boolean mounted) {
-        mRbExternoPropio.setEnabled(mounted);
-        mRbExternoPublico.setEnabled(mounted);
-        mRbCacheExterno.setEnabled(mounted);
+    private void enableOptions(boolean mounted) {
+        rbPersonalExternal.setEnabled(mounted);
+        rbPublicExternal.setEnabled(mounted);
+        rbExternalCache.setEnabled(mounted);
     }
 
 }
---
+++ PR188-PermissionsDispatcher/app/src/main/java/es/iessaladillo/pedrojoya/pr188/utils/FileUtils.java
@@ -0,0 +1,32 @@
+package es.iessaladillo.pedrojoya.pr188.utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileUtils {
+
+    private static final int BUFFER_SIZE = 1000;
+
+    private FileUtils() {
+    }
+
+    public static void copyFile(InputStream inputStream, File outputFile) throws IOException {
+        BufferedInputStream reader;
+        reader = new BufferedInputStream(inputStream);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        BufferedOutputStream writer = new BufferedOutputStream(outputStream);
+        byte[] array = new byte[BUFFER_SIZE];
+        int read = reader.read(array);
+        while (read > 0) {
+            writer.write(array, 0, read);
+            read = reader.read(array);
+        }
+        writer.close();
+        reader.close();
+    }
+
+}
