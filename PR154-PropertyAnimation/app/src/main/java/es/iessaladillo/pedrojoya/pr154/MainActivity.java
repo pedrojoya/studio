27c848900db27b13f83a3db012fa5eb1a899b434
pedrojoya <pedrojoya@gmail.com>
2017-09-27 23:37:49 +0200

    Upgrade version

--- PR154-PropertyAnimation/app/src/main/java/es/iessaladillo/pedrojoya/pr154/MainActivity.java
+++ PR154-PropertyAnimation/app/src/main/java/es/iessaladillo/pedrojoya/pr154/MainActivity.java
@@ -6,6 +6,7 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.AnimatorRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
@@ -17,79 +18,79 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int NUM_XML_ANIMATIONS = 16;
 
-    private ImageView imgImagen;
-    private final int[] resIdsAnimaciones = {R.animator.scale, R.animator.translate, R.animator
-            .translate_y, R.animator.rotate, R.animator.rotate_x, R.animator.rotate_y, R.animator
-                                                     .alpha, R.animator.set, R.animator
-                                                     .secuencia, R.animator.rotate_repeat, R
-                                                     .animator.rotate_repeat_reverse, R.animator
-                                                     .rotate_anticipate, R.animator
-                                                     .rotate_overshoot, R.animator
-                                                     .rotate_anticipate_overshoot, R.animator
-                                                     .rotate_cycle, R.animator.rotate_bounce};
+    private ImageView imgPhoto;
+    @SuppressWarnings("FieldCanBeLocal")
+    private Spinner spnAnimation;
+
+    private final @AnimatorRes int[] animationsResIds = {R.animator.scale, R.animator.translate, R.animator
+            .translate_y, R.animator.rotate, R.animator.rotate_x, R.animator.rotate_y, R.animator.alpha, R.animator.set, R.animator.secuencia, R.animator.rotate_repeat, R.animator.rotate_repeat_reverse, R.animator.rotate_anticipate, R.animator.rotate_overshoot, R.animator.rotate_anticipate_overshoot, R.animator.rotate_cycle, R.animator.rotate_bounce};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initVistas();
+        initViews();
     }
 
-    // Obtiene e inicializa las vistas.
-    private void initVistas() {
-        Spinner spnAnimacion = (Spinner) findViewById(R.id.spnAnimacion);
-        if (spnAnimacion != null) {
-            spnAnimacion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> adapterView, View view, int position,
-                        long id) {
-                    // Se realiza la animación seleccionada.
-                    animar(position);
-                }
+    private void initViews() {
+        spnAnimation = findViewById(R.id.spnAnimation);
+        imgPhoto = findViewById(R.id.imgPhoto);
 
-                @Override
-                public void onNothingSelected(AdapterView<?> adapterView) {
+        spnAnimation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position,
+                    long id) {
+                animate(position);
+            }
 
-                }
-            });
-        }
-        imgImagen = (ImageView) findViewById(R.id.imgImagen);
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
-    // Carga y realizar la animación correspondiente a dicha posición.
-    private void animar(int position) {
+    private void animate(int position) {
         if (position < NUM_XML_ANIMATIONS) {
-            Animator anim = AnimatorInflater.loadAnimator(this, resIdsAnimaciones[position]);
-            anim.setTarget(imgImagen);
-            anim.start();
+            startAnimation(animationsResIds[position]);
         } else {
-            imgImagen.setImageDrawable(null);
-            ObjectAnimator animador = ObjectAnimator.ofObject(imgImagen, "backgroundColor",
-                    new ArgbEvaluator(), Color.RED, Color.GREEN);
-            animador.setDuration(4000);
-            animador.setInterpolator(new LinearInterpolator());
-            animador.addListener(new Animator.AnimatorListener() {
-                @Override
-                public void onAnimationStart(Animator animator) {
-                }
-
-                @Override
-                public void onAnimationEnd(Animator animator) {
-                    imgImagen.setImageResource(R.drawable.imagen);
-                }
-
-                @Override
-                public void onAnimationCancel(Animator animator) {
-
-                }
-
-                @Override
-                public void onAnimationRepeat(Animator animator) {
-
-                }
-            });
-            animador.start();
+            startObjectAnimator();
         }
+    }
+
+    private void startAnimation(int animationsResId) {
+        Animator anim = AnimatorInflater.loadAnimator(this, animationsResId);
+        anim.setTarget(imgPhoto);
+        anim.start();
+    }
+
+    private void startObjectAnimator() {
+        imgPhoto.setImageDrawable(null);
+        ObjectAnimator animator = ObjectAnimator.ofObject(imgPhoto, "backgroundColor",
+                new ArgbEvaluator(), Color.RED, Color.GREEN);
+        animator.setDuration(4000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                imgPhoto.setImageResource(R.drawable.photo);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+        animator.start();
     }
 
 }
