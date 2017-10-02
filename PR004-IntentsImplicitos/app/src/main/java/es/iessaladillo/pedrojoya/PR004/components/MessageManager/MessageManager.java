d2e9c0258dfd0b46b152b039c7045a6450717209
pedrojoya <pedrojoya@gmail.com>
2017-10-02 17:38:54 +0200

    Upgrade version

--- PR004-IntentsImplicitos/app/src/main/java/es/iessaladillo/pedrojoya/PR004/components/MessageManager/MessageManager.java
+++ PR004-IntentsImplicitos/app/src/main/java/es/iessaladillo/pedrojoya/PR004/components/MessageManager/MessageManager.java
@@ -1,10 +1,13 @@
 package es.iessaladillo.pedrojoya.PR004.components.MessageManager;
 
 import android.view.View;
+import android.widget.Toast;
 
 @SuppressWarnings("unused")
 public interface MessageManager {
 
-    void showMessage(View refView, String message);
+    default void showMessage(View refView, String message) {
+        Toast.makeText(refView.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
 
 }
--- PR004-IntentsImplicitos/app/src/main/java/es/iessaladillo/pedrojoya/PR004/components/MessageManager/ToastMessageManager.java
+++ PR004-IntentsImplicitos/app/src/main/java/es/iessaladillo/pedrojoya/PR004/components/MessageManager/ToastMessageManager.java
@@ -1,11 +1,3 @@
 package es.iessaladillo.pedrojoya.PR004.components.MessageManager;
 
-import android.view.View;
-import android.widget.Toast;
-
-public class ToastMessageManager implements MessageManager {
-    @Override
-    public void showMessage(View refView, String message) {
-            Toast.makeText(refView.getContext(), message, Toast.LENGTH_SHORT).show();
-    }
-}
+public class ToastMessageManager implements MessageManager {}
