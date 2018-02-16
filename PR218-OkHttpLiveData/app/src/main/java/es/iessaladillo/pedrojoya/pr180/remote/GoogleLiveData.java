302149f1da6e8cfc94458acb6d81f88d9337a830
pedrojoya <pedrojoya@gmail.com>
2018-02-16 21:25:32 +0100

    Delete unused projects

--- PR218-OkHttpLiveData/app/src/main/java/es/iessaladillo/pedrojoya/pr180/remote/GoogleLiveData.java
+++
@@ -1,8 +0,0 @@
-package es.iessaladillo.pedrojoya.pr180.remote;
-
-import es.iessaladillo.pedrojoya.pr180.utils.SingleLiveEvent;
-
-public class GoogleLiveData extends SingleLiveEvent<String> {
-
-
-}
