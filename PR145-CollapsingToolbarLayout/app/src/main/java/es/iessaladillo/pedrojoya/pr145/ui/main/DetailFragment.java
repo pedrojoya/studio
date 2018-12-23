c62d7c800d805fd3b86e6df25c848f2cdc69091a
pedrojoya <pedrojoya@gmail.com>
2018-12-23 09:39:07 +0100

    PR145 - Move collapsing toolbar to detail fragment

--- PR145-CollapsingToolbarLayout/app/src/main/java/es/iessaladillo/pedrojoya/pr145/ui/main/DetailFragment.java
+++ PR145-CollapsingToolbarLayout/app/src/main/java/es/iessaladillo/pedrojoya/pr145/ui/main/DetailFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -12,51 +14,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import es.iessaladillo.pedrojoya.pr145.R;
-import es.iessaladillo.pedrojoya.pr145.utils.FragmentUtils;
 
-public class MainFragment extends Fragment {
+public class DetailFragment extends Fragment {
 
-    public MainFragment() { }
+    public DetailFragment() { }
 
     @SuppressWarnings("WeakerAccess")
-    public static MainFragment newInstance() {
-        return new MainFragment();
+    public static DetailFragment newInstance() {
+        return new DetailFragment();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_detail, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        setupViews(requireView());
-    }
-
-    private void setupViews(View view) {
-        setupToolbar(view);
-        ViewCompat.requireViewById(view, R.id.btnShowDetail).setOnClickListener(v ->
-            navigateToDetail());
+        setupToolbar(requireView());
     }
 
     private void setupToolbar(View view) {
         Toolbar toolbar = ViewCompat.requireViewById(view, R.id.toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = ViewCompat.requireViewById(view,
+            R.id.collapsingToolbarLayout);
+
+        collapsingToolbarLayout.setTitle(getString(R.string.detail_title));
         ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
         ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
         if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
-    private void navigateToDetail() {
-        FragmentUtils.replaceFragmentAddToBackstack(requireFragmentManager(), R.id.flContent,
-            DetailFragment.newInstance(), DetailFragment.class.getSimpleName(), DetailFragment
-                .class.getSimpleName(), FragmentTransaction.TRANSIT_NONE);
-    }
 
 }
--- PR145-CollapsingToolbarLayout/app/src/main/java/es/iessaladillo/pedrojoya/pr145/ui/main/MainActivity.java
+++ PR145-CollapsingToolbarLayout/app/src/main/java/es/iessaladillo/pedrojoya/pr145/ui/main/MainActivity.java
@@ -5,11 +5,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
 import es.iessaladillo.pedrojoya.pr145.R;
 import es.iessaladillo.pedrojoya.pr145.utils.FragmentUtils;
 
@@ -20,19 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupToolbar();
         if (savedInstanceState == null) {
             loadInitialFragment();
         }
-    }
-
-    private void setupToolbar() {
-        Toolbar toolbar = ActivityCompat.requireViewById(this, R.id.toolbar);
-        CollapsingToolbarLayout collapsingToolbarLayout = ActivityCompat.requireViewById(this,
-            R.id.collapsingToolbarLayout);
-
-        setSupportActionBar(toolbar);
-        collapsingToolbarLayout.setTitle(getString(R.string.main_title));
     }
 
     private void loadInitialFragment() {
@@ -57,6 +43,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void showSettings() {
         Toast.makeText(this, getString(R.string.main_mnuSettings), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
 }
