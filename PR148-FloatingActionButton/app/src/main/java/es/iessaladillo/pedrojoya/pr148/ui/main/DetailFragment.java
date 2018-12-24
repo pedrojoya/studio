37cf80090ae966e1c21d3f9eb29c60d6061b7a92
pedrojoya <pedrojoya@gmail.com>
2018-12-24 10:11:13 +0100

    PR148 - Move collapsing toolbar to detail fragment

---
+++ PR148-FloatingActionButton/app/src/main/java/es/iessaladillo/pedrojoya/pr148/ui/main/DetailFragment.java
@@ -0,0 +1,98 @@
+package es.iessaladillo.pedrojoya.pr148.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.ViewCompat;
+import androidx.fragment.app.Fragment;
+import es.iessaladillo.pedrojoya.pr148.R;
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment() { }
+
+    @SuppressWarnings("WeakerAccess")
+    public static DetailFragment newInstance() {
+        return new DetailFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setupViews();
+    }
+
+    private void setupViews() {
+        setupToolbar(requireView());
+        setupFab(requireView());
+    }
+
+    private void setupToolbar(View view) {
+        Toolbar toolbar = ViewCompat.requireViewById(view, R.id.toolbar);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = ViewCompat.requireViewById(view,
+            R.id.collapsingToolbarLayout);
+        // TODO: We disable de title because we can't make it collapse properly
+        collapsingToolbarLayout.setTitleEnabled(false);
+        collapsingToolbarLayout.setTitle(getString(R.string.detail_title));
+        ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupFab(View view) {
+        FloatingActionButton fab = ViewCompat.requireViewById(view, R.id.fab);
+        fab.setOnClickListener(v -> save());
+    }
+
+    private void save() {
+        Toast.makeText(requireContext(), getString(R.string.detail_fab_clicked), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        enableDrawSystemBar();
+    }
+
+    private void enableDrawSystemBar() {
+        Window window = requireActivity().getWindow();
+        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(android.R.color.transparent));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        disableDrawSystemBar();
+    }
+
+    private void disableDrawSystemBar() {
+        Window window = requireActivity().getWindow();
+        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //window.clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+    }
+}
--- PR148-FloatingActionButton/app/src/main/java/es/iessaladillo/pedrojoya/pr148/ui/main/MainActivity.java
+++ PR148-FloatingActionButton/app/src/main/java/es/iessaladillo/pedrojoya/pr148/ui/main/MainActivity.java
@@ -3,14 +3,10 @@ package es.iessaladillo.pedrojoya.pr148.ui.main;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Toast;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
 import es.iessaladillo.pedrojoya.pr148.R;
 import es.iessaladillo.pedrojoya.pr148.utils.FragmentUtils;
 
@@ -21,38 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupViews();
         if (savedInstanceState == null) {
             loadInitialFragment();
         }
-    }
-
-    private void setupViews() {
-        setupToolbar();
-        setupFab();
-    }
-
-    private void setupToolbar() {
-        Toolbar toolbar = ActivityCompat.requireViewById(this, R.id.toolbar);
-        CollapsingToolbarLayout collapsingToolbarLayout = ActivityCompat.requireViewById(this,
-            R.id.collapsingToolbarLayout);
-
-        setSupportActionBar(toolbar);
-        collapsingToolbarLayout.setTitle(getTitle());
-    }
-
-    private void setupFab() {
-        FloatingActionButton fab = ActivityCompat.requireViewById(this, R.id.fab);
-        fab.setOnClickListener(v -> save());
+        Window window = getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
     }
 
     private void loadInitialFragment() {
         FragmentUtils.replaceFragment(getSupportFragmentManager(), R.id.flContent,
-            MainFragment.newInstance(), MainFragment.class.getSimpleName());
-    }
-
-    private void save() {
-        Toast.makeText(this, getString(R.string.main_fab_clicked), Toast.LENGTH_SHORT).show();
+                MainFragment.newInstance(), MainFragment.class.getSimpleName());
     }
 
     @Override
@@ -72,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void showSettings() {
         Toast.makeText(this, getString(R.string.main_mnuSettings), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
 }
